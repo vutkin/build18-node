e389364b9d70972266ad603927e5d06dc3a96c09
Viktor Utkin <vutkin@users.noreply.github.com>
2019-01-24 14:44:06 +0300

    Update node-demo.js

--- node-demo.js
+++ node-demo.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 const handler = (req, res) => {
     console.log('Server received request.');
-    figlet('Hello Build 2019!!!', (err, data) => {
+    figlet('Hello Build 2020!!!', (err, data) => {
       if (err) {
             console.log('Something went wrong... ');
             console.dir(err);
